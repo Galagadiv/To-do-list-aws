ddc93e7ff21cf029569cfa387fd6b444d86ab8c8
Galagadiv <sobolia37@gmail.com>
2025-05-30 17:08:02 +0300

    fix: base fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/task-list/`,
+  base: ``,
   build: {
     outDir: "docs",
   },
